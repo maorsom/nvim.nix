61e5dad6b7356dad0bc176571262a0aa8f5900ff
Maor Somech <maorsom@gmail.com>
2025-05-29 23:23:28 +0300

    telescope

---
+++ config/plugins/telescope.nix
@@ -0,0 +1,149 @@
+{
+  plugins.telescope = {
+    enable = true;
+    extensions = {
+      fzf-native = {
+        enable = true;
+      };
+    };
+    settings = {
+      defaults = {
+        file_ignore_patterns = [
+          "^.git/"
+          "^.mypy_cache/"
+          "^__pycache__/"
+          "^output/"
+          "^data/"
+          "%.ipynb"
+        ];
+        set_env.COLORTERM = "truecolor";
+        layout_config = {
+          horizontal = {
+            prompt_position = "top";
+          };
+        };
+        sorting_strategy = "ascending";
+      };
+    };
+    keymaps = {
+      "<leader>ff" = {
+        action = "find_files";
+        options = {
+          desc = "Find project files";
+        };
+      };
+      "<leader>fg" = {
+        action = "live_grep";
+        options = {
+          desc = "Grep (root dir)";
+        };
+      };
+      "<leader>fh" = {
+        action = "command_history";
+        options = {
+          desc = "Command History";
+        };
+      };
+      "<leader>b" = {
+        action = "buffers";
+        options = {
+          desc = "+buffer";
+        };
+      };
+      "<leader>fR" = {
+        action = "resume";
+        options = {
+          desc = "Resume";
+        };
+      };
+      "<leader>gf" = {
+        action = "git_files";
+        options = {
+          desc = "Search git files";
+        };
+      };
+      "<leader>gc" = {
+        action = "git_commits";
+        options = {
+          desc = "Commits";
+        };
+      };
+      "<leader>gs" = {
+        action = "git_status";
+        options = {
+          desc = "Status";
+        };
+      };
+      "<leader>sa" = {
+        action = "autocommands";
+        options = {
+          desc = "Auto Commands";
+        };
+      };
+      "<leader>sb" = {
+        action = "current_buffer_fuzzy_find";
+        options = {
+          desc = "Buffer";
+        };
+      };
+      "<leader>fd" = {
+        action = "diagnostics";
+        options = {
+          desc = "Workspace diagnostics";
+        };
+      };
+      "<leader>ft" = {
+        action = "help_tags";
+        options = {
+          desc = "Help pages";
+        };
+      };
+      "<leader>sh" = {
+        action = "highlights";
+        options = {
+          desc = "Search Highlight Groups";
+        };
+      };
+      "<leader>sk" = {
+        action = "keymaps";
+        options = {
+          desc = "Keymaps";
+        };
+      };
+      "<leader>sM" = {
+        action = "man_pages";
+        options = {
+          desc = "Man pages";
+        };
+      };
+      "<leader>sm" = {
+        action = "marks";
+        options = {
+          desc = "Jump to Mark";
+        };
+      };
+      "<leader>so" = {
+        action = "vim_options";
+        options = {
+          desc = "Options";
+        };
+      };
+      "<leader>sR" = {
+        action = "resume";
+        options = {
+          desc = "Resume";
+        };
+      };
+    };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>sd";
+      action = "<cmd>Telescope diagnostics bufnr=0<cr>";
+      options = {
+        desc = "Document diagnostics";
+      };
+    }
+  ];
+}
